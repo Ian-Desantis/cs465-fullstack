826eca849f9684e69f8870bbf2ed73a2d6fed246
ian.desantis <ian.desantis@snhu.edu>
2022-10-08 15:48:09 -0400

    Module 5 work completed. DB working as expected. Click the images to see raw json

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,29 @@
-var fs = require('fs'); 
+const request = require("request");
+const apiOptions = {
+  server: "http://localhost:3000",
+};
 
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+// var fs = require('fs'); 
+//var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+const renderTravelList = (rec, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + " Travel";
+  
+    if (!(responseBody instanceof Array)) {
+        message = "API lookup error";
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = "No trips exist in database";
+        }
+    }
+    res.render("travel", {
+        title: pageTitle,
+        trips: responseBody,
+        message,
+    });
+  };
 
 /* GET travel view */
 const travel = (req, res) => {
@@ -10,3 +33,25 @@ const travel = (req, res) => {
    module.exports = {
     travel
    };
+
+// get travel list view
+const travelList = (req, res) => {
+    const path = "/api/trips";
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: "GET",
+        json: {},
+    };
+  
+    console.info(" >> travelcontroller.travelList calling" + requestOptions.url);
+    request(requestOptions, (err, { statusCode }, body) => {
+        if (err) {
+            console.error(err);
+        }
+        renderTravelList(req, res, body);
+    });
+  };
+
+  module.exports = {
+    travelList,
+  };
